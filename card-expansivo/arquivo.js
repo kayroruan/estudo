4db455b265a894a7b592dbfb38479b54f769b0a5
kayroruan <ruankayrov1@gmail.com>
2022-08-01 06:59:01 -0300

    .

--- card-expansivo/arquivo.js
+++ card-expansivo/arquivo.js
@@ -1,16 +1,62 @@
-function expande1(){
-    x=document.getElementById("card1");
-    if(x.style.height=="188px")
-        x.style.height = 440 + 'px';
-    else
-        x.style.height = 188 + 'px';
+var x = true;
+$('#card1').click(function() {
+  if(x) {
+    x = false;
+    $(this).animate({
+        height : "440px",
+    }, 1000);
+    $('#c1').css('display','block');
+    $('#seta1').css('transform', 'rotate(90deg)');
 
-}
+  } else {
+    x = true;
+    $(this).animate({
+      height : "188px",
+    }, 1000);
+    $('#c1').css('display','none')
+    $('#seta1').css('transform', 'rotate(0deg)');
+  }
 
-function expande2(){
-    x=document.getElementById("card2");
-    if(x.style.height=="188px")
-        x.style.height = 440 + 'px';
-    else
-        x.style.height = 188 + 'px';
-}
+});
+
+var x = true;
+$('#card2').click(function() {
+  if(x) {
+    x = false;
+    $(this).animate({
+        height : "440px",
+    }, 1000);
+    $('#c2').css('display','block');
+    $('#seta2').css('transform', 'rotate(90deg)');
+
+  } else {
+    x = true;
+    $(this).animate({
+      height : "188px",
+    }, 1000);
+    $('#c2').css('display','none')
+    $('#seta2').css('transform', 'rotate(0deg)');
+  }
+
+});
+
+var x = true;
+$('#card3').click(function() {
+  if(x) {
+    x = false;
+    $(this).animate({
+        height : "440px",
+    }, 1000);
+    $('#c3').css('display','block');
+    $('#seta3').css('transform', 'rotate(90deg)');
+
+  } else {
+    x = true;
+    $(this).animate({
+      height : "188px",
+    }, 1000);
+    $('#c3').css('display','none')
+    $('#seta3').css('transform', 'rotate(0deg)');
+  }
+
+});
