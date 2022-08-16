fe596737c537c08b75320c42c307b4e80b2259da
kayroruan <ruankayrov1@gmail.com>
2022-08-15 21:19:40 -0300

    .

--- card-expansivo/arquivo.js
+++ card-expansivo/arquivo.js
@@ -1,10 +1,11 @@
 var x = true;
 $('#card1').click(function() {
+  if ($(window).width() > 768)
   if(x) {
     x = false;
     $(this).animate({
         height : "440px",
-    }, 1000);
+    }, 500);
     $('#c1').css('display','block');
     $('#seta1').css('transform', 'rotate(90deg)');
 
@@ -12,7 +13,7 @@ $('#card1').click(function() {
     x = true;
     $(this).animate({
       height : "188px",
-    }, 1000);
+    }, 500);
     $('#c1').css('display','none')
     $('#seta1').css('transform', 'rotate(0deg)');
   }
@@ -21,11 +22,12 @@ $('#card1').click(function() {
 
 var x = true;
 $('#card2').click(function() {
+  if ($(window).width() > 768)
   if(x) {
     x = false;
     $(this).animate({
         height : "440px",
-    }, 1000);
+    }, 500);
     $('#c2').css('display','block');
     $('#seta2').css('transform', 'rotate(90deg)');
 
@@ -33,7 +35,7 @@ $('#card2').click(function() {
     x = true;
     $(this).animate({
       height : "188px",
-    }, 1000);
+    }, 500);
     $('#c2').css('display','none')
     $('#seta2').css('transform', 'rotate(0deg)');
   }
@@ -42,11 +44,12 @@ $('#card2').click(function() {
 
 var x = true;
 $('#card3').click(function() {
+  if ($(window).width() > 768)
   if(x) {
     x = false;
     $(this).animate({
         height : "440px",
-    }, 1000);
+    },500);
     $('#c3').css('display','block');
     $('#seta3').css('transform', 'rotate(90deg)');
 
@@ -54,7 +57,72 @@ $('#card3').click(function() {
     x = true;
     $(this).animate({
       height : "188px",
-    }, 1000);
+    }, 500);
+    $('#c3').css('display','none')
+    $('#seta3').css('transform', 'rotate(0deg)');
+  }
+
+});
+
+$('#card1').click(function() {
+  if ($(window).width() < 768)
+  if(x) {
+    x = false;
+    $(this).animate({
+        height : "173px",
+    }, 500);
+    $('#c1').css('display','block');
+    $('#seta1').css('transform', 'rotate(90deg)');
+
+  } else {
+    x = true;
+    $(this).animate({
+      height : "69px",
+    }, 500);
+    $('#c1').css('display','none')
+    $('#seta1').css('transform', 'rotate(0deg)');
+  }
+
+});
+
+var x = true;
+$('#card2').click(function() {
+  if ($(window).width() < 768)
+  if(x) {
+    x = false;
+    $(this).animate({
+        height : "173px",
+    }, 500);
+    $('#c2').css('display','block');
+    $('#seta2').css('transform', 'rotate(90deg)');
+
+  } else {
+    x = true;
+    $(this).animate({
+      height : "69px",
+    }, 500);
+    $('#c2').css('display','none')
+    $('#seta2').css('transform', 'rotate(0deg)');
+  }
+
+});
+
+var x = true;
+$('#card3').click(function() {
+  if ($(window).width() < 768)
+  if(x) {
+    x = false;
+    $(this).animate({
+        height : "173px",
+    },500);
+    $('#c3').css('display','block');
+    $('#seta3').css('transform', 'rotate(90deg)');
+
+  } else {
+    x = true;
+    $(this).animate({
+      height : "69px",
+    }, 500);
     $('#c3').css('display','none')
     $('#seta3').css('transform', 'rotate(0deg)');
   }
